2fb98921b05390c75fbff0df720d4168c3429520
Amal Francis <40779306+RedCoder7@users.noreply.github.com>
2018-07-06 07:36:07 -0700

    First Draft

--- InsertionSortingAlgo/main.c
+++ InsertionSortingAlgo/main.c
@@ -1,8 +1,45 @@
+//This code is an implementation of insertion sort.
 #include <stdio.h>
-#include <stdlib.h>
-
+void pushSort(int a[], int num,int prev)
+{
+  int temp = a[num];
+  int sb = prev+1;  //sb = starting block
+  while(num > sb)
+  {
+    a[num] = a[num-1];
+    num--;
+  }
+  a[sb] = temp;
+    puts("Output array is:");
+  int i;
+  for(i =0 ; i < 5; i++ )
+  {
+    printf("%d ",a[i]);
+  }
+}
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+  int a[] = {19,700,-10,99,2};
+  int x, num, prev, len;
+  x = 0;
+  len = 5;
+  num = 1;
+  prev = num - 1;
+  for(x = 1; x < len ; x++)
+  {
+    while(prev != -1)
+    {
+      if(a[num] < a[prev])
+      {
+        prev--;
+        printf("prev is %d", prev);
+      }
+      else
+        break;
+    }
+    pushSort(a,num,prev);
+    num++;
+    prev = num - 1;
+  }
+  return 0;
 }
