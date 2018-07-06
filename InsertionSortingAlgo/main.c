68310eb6a73140971f2b068b441a61d072a3bb78
Amal Francis <40779306+RedCoder7@users.noreply.github.com>
2018-07-06 10:15:32 -0700

    Mod3
    Added some analysis comments regarding the code. Also cleaned it.

--- InsertionSortingAlgo/main.c
+++ InsertionSortingAlgo/main.c
@@ -1,10 +1,19 @@
-//dsffsfThis code is an implementation of insertion sort.
+/*This code is an implementation of insertion sort.
+This is not an efficient implementation of the Insertion sort.
+Complexity: O(n^3)
+First check all elements then push at the end:-In this technique, the 'a[num]' is the element of interest that is compared with the
+elements to the left of its position. If the elements are greater then keep on checking until you reach the
+start of the array. If any element is smaller than a[num] then push the bigger elements to the right and place
+a[num] at the right place.
+There is one more issue with this code, if the a[num] is greater than the element to the left of it in the very
+first iteration, then ideally it should never call pushSort(). But it does, thankfully pushSort() function takes
+care of this case.
+A better implementation is here: https://www.geeksforgeeks.org/insertion-sort/
+*/
 #include <stdio.h>
 void printArr(int arr[])
 {
-    int i,length;
-    length = sizeof(arr)/sizeof(int);
-    printf("\nLength  is:%d",length);
+    int i;
     printf("\nOutput array is:");
     for(i =0 ; i < 5; i++ )
     {
@@ -27,12 +36,8 @@ int main()
 {
   int a[] = {5,3,4,1,2};
   int  num, prev, len;
-
-//  len = 5;
     len = sizeof(a)/sizeof(int);
     printf("\nLength  is:%d",len);
-//  num = 1;
-//  prev = num - 1;
   for(num = 1; num < len ; num++)
   {
     prev = num - 1;
@@ -41,14 +46,12 @@ int main()
       if(a[num] < a[prev])
       {
         prev--;
-        printf("prev is %d", prev);
       }
       else
         break;
     }
     pushSort(a,num,prev);
-//    num++;
-//    prev = num - 1;
+   prev = num - 1;
   }
   printArr(a);
   return 0;
